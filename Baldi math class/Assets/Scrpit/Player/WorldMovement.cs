01ff50fd5e362ba15bcf4bdb7ac52cad883c557e
Nanachi <b01077016039@gmail.com>
2018-06-13 21:11:09 +0900

    Unity Practice - Math Class 1.0
    Map, Player

---
+++ Baldi math class/Assets/Scrpit/Player/WorldMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMovement : MonoBehaviour
+{
+    float MoveSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        MoveSpeed = 0.1f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(-MoveSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, -MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(MoveSpeed, 0, 0);
+        }
+
+
+    }
+}
