9289b2a4f06fbb9420d9aadce90e8cc482a8ffa8
Jinsab <kimjinou116@naver.com>
2019-07-12 14:12:21 +0900

    프로그래머스 정렬-가장 큰 수

---
+++ verybignumber.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool desc(const string &a, const string &b) {
+	return a + b > b + a ? true : false;
+}
+
+string solution(vector<int> numbers) {
+	string answer = "";
+	vector<string> strArr;
+
+	for (int i = 0; i < numbers.size(); i++)
+		strArr.push_back(to_string(numbers.at(i)));
+
+	sort(strArr.begin(), strArr.end(), desc);
+
+	for (string str : strArr)
+		answer += str;
+
+	if (answer[0] == '0')
+		return "0";
+
+	return answer;
+}
