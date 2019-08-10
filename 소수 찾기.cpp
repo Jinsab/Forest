ca808a399f8ad13b0f0c215d089c15289a9652fb
Nanachi <36787932+Jinsab@users.noreply.github.com>
2019-08-10 10:59:44 +0900

    프로그래머스 - 소수 찾기

---
+++ 소수 찾기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+
+	bool check[1000001] = { false, };
+
+	for (int i = 2; i <= n; i++) {
+		if (!check[i]) answer++;
+		for (int j = i; j <= n; j += i) check[j] = true;
+	}
+
+	return answer;
+}
